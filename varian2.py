90d33463b0d7ca69db0c0ad94180b317716a049a
monax120 <72456614+monax120@users.noreply.github.com>
2020-10-09 14:21:01 +0200

    Initial commit

---
+++ varian2.py
@@ -0,0 +1,53 @@
+from random import randint
+
+numberOfTries = 0
+
+def numberGuess(startRange, endRange):
+    if startRange > endRange:
+        return True
+    
+    randRamge = randint(startRange, endRange)
+   
+    global numberOfTries
+    
+    print('Is your number ', randRamge,'?', end=' ')
+    user = input().lower()
+    
+    if user == 'y':
+        print("Yey, I've guessed your number in ", numberOfTries," tries, not bad at all!")
+        
+    elif user == 'n':
+        
+        print('Is your number Greater than ', randRamge, ' ?', end=' ')
+        if startRange == endRange:
+                print('Is your number Greater than ', startRange, ' ?', end=' ')    
+        user = input().lower()
+        print(user)
+        if user == 'y':
+            numberOfTries = numberOfTries + 1
+            return numberGuess(randRamge+1, endRange)
+        elif user == 'n':
+            numberOfTries = numberOfTries + 1
+            return numberGuess(startRange, randRamge-1)
+        else:
+            print('Please answer only "Y" or "N"')
+            return numberGuess(startRange, endRange)
+            
+    else: 
+        print('Please answer only "Y" or "N"')
+        return numberGuess(startRange, endRange)
+        
+if __name__ == '__main__':
+    print('Number guessing game, Guess a number in range (1 to 100)')
+    startRange = 1
+    endRange = 100
+    
+    out = numberGuess(startRange, endRange)
+    
+    if out:
+        print('Bad choices')
+            
+            
+        
+    
+    
---
+++ variant1.py
@@ -0,0 +1,51 @@
+
+numberOfTries = 0
+
+def numberGuess(startRange, endRange):
+    if startRange > endRange:
+        return True
+    
+    midRange = (startRange + endRange)//2
+   
+    global numberOfTries
+    
+    print('Is your number ', midRange,'?', end=' ')
+    user = input().lower()
+    
+    if user == 'y':
+        print("Yey, I've guessed your number in ", numberOfTries," tries, not bad at all!")
+        
+    elif user == 'n':
+        
+        print('Is your number Greater than ', midRange, ' ?', end=' ')
+        
+        user = input().lower()
+        print(user)
+        if user == 'y':
+            numberOfTries = numberOfTries + 1
+            return numberGuess(midRange+1, endRange)
+        elif user == 'n':
+            numberOfTries = numberOfTries + 1
+            return numberGuess(startRange, midRange-1)
+        else:
+            print('Please answer only "Y" or "N"')
+            return numberGuess(startRange, endRange)
+            
+    else: 
+        print('Please answer only "Y" or "N"')
+        return numberGuess(startRange, endRange)
+        
+if __name__ == '__main__':
+    print('Number guessing game, Guess a number in range (1 to 100)')
+    startRange = 1
+    endRange = 100
+    
+    out = numberGuess(startRange, endRange)
+    
+    if out:
+        print('Bad choices')
+            
+            
+        
+    
+    
